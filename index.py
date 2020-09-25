8c0255c8f41ed14a0dfe9108b2bd5129c0785f6e
Konrad Iturbe <mail@chernowii.com>
2020-09-25 12:10:28 +0200

    Much faster, and better

--- index.py
+++ index.py
@@ -3,28 +3,15 @@ from flask import request
 from goprocam import GoProCamera, constants
 
 app = Flask(__name__)
-gopro = GoProCamera.GoPro()
-
+gopro = GoProCamera.GoPro(ip_address=GoProCamera.GoPro.getWebcamIP("enp0s20u2"), camera=constants.gpcontrol, webcam_device="enp0s20u2")
 def format_media():
-    data = []
     media=gopro.listMedia(True, True)
-    for m in media:
-        duration=gopro.parse_value("media_size",int(m[2]))
-        if m[1].endswith(".MP4"):
-            duration=gopro.parse_value("video_left",int(gopro.getVideoInfo("dur",m[1],m[0])))
-        resultData = {'folder': m[0],
-                      'filename': m[1],
-					  'dur': duration,
-                      'purl': "http://10.5.5.9/gp/gpMediaMetadata?p=/" + m[0] + "/" + m[1]}
-        data.append(resultData)
-    return data	
+    return media	
 def gp_info():
 	info = ""
 	info = info + " " + gopro.infoCamera("model_name")
 	info = info + " " + gopro.infoCamera("ap_ssid")
 	info = info + " " + gopro.infoCamera("firmware_version")
-	info = info + " " + gopro.parse_value("mode", gopro.getStatus(constants.Status.Status, constants.Status.STATUS.Mode))
-	info = info + " " + gopro.parse_value("sub_mode", gopro.getStatus(constants.Status.Status, constants.Status.STATUS.SubMode))
 	return info
 @app.route('/')
 def media():
