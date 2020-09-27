fe9e23f69e33bae266d35ca8f659df254ec5996c
Konrad Iturbe <mail@chernowii.com>
2020-09-27 12:21:16 +0200

    GoPro Dashboard first MVP

--- README.md
+++ README.md
@@ -1,9 +1,29 @@
 # GoProDashboard
 
-A Python webapp to interface with GoPro cameras over wifi. 
+A Python webapp to interface with GoPro cameras over USB (GoPro Connect mode/Webcam mode). 
 
 Currently supports viewing media
 
-![](http://i.imgur.com/S0z0Kql.png)
+This project was born out of a necessity to offload media via WiFi while on a trip due to a broken SD card reader.
 
-![](http://i.imgur.com/FjOgj5q.png)
+4 years later, GoPro introduced a mode that allows gpControl APIs to be used using USB Ethernet, thus giving us the APIs necessary to index, download and delete media from the camera programmatically.
+
+![](https://i.imgur.com/u6MBty4.jpg)
+
+### Running:
+
+Plug your GoPro camera, make sure it's in GoPro Connect mode, and wait a few seconds for the RNDIS lease to occur.
+
+````
+pipenv shell
+python main.py
+````
+
+### Configuring:
+
+`MEDIA_DOWNLOAD_DIR`: this is where the media will be downloaded
+`INTERFACE`: GoPro USB Ethernet interface (run ifconfig if you're not sure)
+`DEBUG`
+
+.env.example provides default values, code will look for .env
+
--- index.py
+++ index.py
@@ -1,9 +1,23 @@
 from flask import Flask, render_template, jsonify
 from flask import request
 from goprocam import GoProCamera, constants
+from signal import signal, SIGINT
+from dotenv import load_dotenv
+import threading
+import json
+import traceback
+import os
 
 app = Flask(__name__)
-gopro = GoProCamera.GoPro(ip_address=GoProCamera.GoPro.getWebcamIP("enp0s20u2"), camera=constants.gpcontrol, webcam_device="enp0s20u2")
+load_dotenv(dotenv_path='.env')
+
+gopro = GoProCamera.GoPro(ip_address=GoProCamera.GoPro.getWebcamIP(os.getenv("INTERFACE")), camera=constants.gpcontrol, webcam_device=os.getenv("INTERFACE"))
+def handler(s, f):
+    try:
+        gopro.gpTurbo(constants.off)
+    except: pass
+    quit()
+
 def format_media():
     media=gopro.listMedia(True, True)
     return media	
@@ -15,12 +29,26 @@ def gp_info():
 	return info
 @app.route('/')
 def media():
-    searchResults = format_media()
-    return render_template('index.html', searchResults=searchResults,goproinfo=gp_info())
+    media = format_media()
+    return render_template('index.html', media=media,goproinfo=gp_info(), ip_address=GoProCamera.GoPro.getWebcamIP(os.getenv("INTERFACE")))
+
+@app.route("/ping")
+def ping():
+    if len(gopro.infoCamera()) == 0:
+        return "not ok"
+    return "ok"
+@app.route("/download")
+def download():
+    x = threading.Thread(target=gopro.downloadMedia, args=(request.args.get('folder'),request.args.get('filename'), os.getenv("MEDIA_DOWNLOAD_DIR") + "/" + request.args.get('folder') + "_" +request.args.get('filename'),))
+    x.start()
+    return ("", 200)
 
 @app.route('/delete')
 def delete_media():
 	gopro.deleteFile(request.args.get('folder'),request.args.get('filename'))
 	return media()
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    signal(SIGINT, handler)
+    gopro.gpTurbo(constants.on)
+    app.run(debug=True if os.getenv("DEBUG") == "true" else False)
